06c9457f2fc0ef1d590b3735ca80255d26c6364c
nbubna <nathan@esha.com>
2014-03-18 21:31:28 -0700

    all new css-based rewrite

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -1,34 +1,53 @@
-(function(Eventi, HTML) {
+(function(Eventi, document, location) {
     'use strict';
 
-    HTML.head.add('style').innerHTML =
-        '[view] { display: none } '+
-        '[view].shown { display: block; }';
-
-    Eventi.on('location', function() {
-        var url = location.href,
-            none = [],
-            show = [],
-            hide = [];
-
-        HTML.query('[view]').each(function(el) {
-            var on = el.getAttribute('view');
-            if (!on) {
-                none.push(el);
-            } else if (url.indexOf(on) >= 0) {
-                show.push(el);
-            } else {
-                hide.push(el);
-            }
+    var views = [];
+    views.define = function(name, re) {
+        views.push({
+            name: name,
+            re: re instanceof RegExp ? re : new RegExp(re || name)
         });
-
-        if (show.length) {
-            hide = hide.concat(none);
-        } else {
-            show = none;
+        views.style.innerHTML += views.rule(name);
+    };
+    views.rule = function(name) {
+        return '.view-'+name+
+             ', .'+name+' .hide-'+name+' { display: none; }\n'+
+               '.'+name+' .view-'+name+' { display: block; }\n'+
+               '.'+name+' span.view-'+name+
+             ', .'+name+' a.view-'+name+
+             ', .'+name+' input.view-'+name+
+             ', .'+name+' button.view-'+name+
+             ', .'+name+' select.view-'+name+
+             ', .'+name+' label.view-'+name+
+             ', .'+name+' img.view-'+name+' { display: inline-block; }\n';
+    };
+    views.update = function(path) {
+        var url = path || location.pathname + location.search + location.hash,
+            start = true;
+        for (var i=0, m=views.length; i<m; i++) {
+            var view = views[i],
+                show = view.re.test(url);
+            document.body.classList.toggle(view.name, show);
+            if (show) {
+                start = false;
+            }
         }
-        HTML.ify(hide).each('classList.remove', 'shown');
-        HTML.ify(show).each('classList.add', 'shown');
-    });
+        document.body.classList.toggle('start', start);
+    };
 
-})(window.Eventi, document.documentElement);
+    var meta = document.querySelector('meta[name=views]');
+    if (meta) {
+        var style = views.style = document.createElement('style'),
+            definitions = meta.getAttribute('content') || '';
+        definitions.split(' ').forEach(function(view) {
+            views.define.apply(views, view.split('='));
+        });
+        views.define('start');
+        document.head.appendChild(style);
+        Object.defineProperty(location, 'views', {value:views});
+        Eventi.on(views, 'location', function updateView(e, path) {
+            views.update(path);
+        });
+    }
+
+})(window.Eventi, document, window.location);
