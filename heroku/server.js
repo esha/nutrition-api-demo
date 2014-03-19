c06614c5e49dae8265b9c05eef3784fc5348d3bb
nbubna <nathan@esha.com>
2014-03-19 15:45:50 -0700

    use express for static files

--- heroku/server.js
+++ heroku/server.js
@@ -1,21 +1,16 @@
 'use strict';
 
-/*
-var statik = require('statik');
-var server = statik.createServer('.');
-server.listen();
-*/
-
-// web.js
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+app.get('/api/', function(req, res) {
   res.send('Hello World!');
 });
+
+app.use(express.static(__dirname + '/'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
