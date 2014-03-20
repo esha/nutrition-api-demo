bda1a3e910d6842a27c7a68aa56f6409afce3593
nbubna <nathan@esha.com>
2014-03-19 17:14:10 -0700

    fresh build

--- dist/server.js
+++ dist/server.js
@@ -2,17 +2,27 @@
 
 var express = require("express");
 var logfmt = require("logfmt");
+var request = require('request')
 var app = express();
+var api = 'http://api.esha.com';
+var key = '2s35wsxke74rcut8jqapbkyf';
+
+function toApi(url) {
+    url = url.replace('/api', api);
+    url += url.indexOf('?') > 0 ? '&' : '?';
+    url += 'apikey='+key;
+    return url;
+}
 
 app.use(logfmt.requestLogger());
 
-app.get('/api/', function(req, res) {
-  res.send('Hello World!');
+app.get('/api/*', function(req, res) {
+   request(toApi(req.originalUrl)).pipe(res);
+});
+app.post('/api/analysis', function(req, res) {
+    req.pipe(request.post(toApi(req.originalUrl))).pipe(res);
 });
 
 app.use(express.static(__dirname + '/'));
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 3000);
