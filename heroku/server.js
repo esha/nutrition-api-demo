f1782bfa40862b902706c069f431d6429a3fbec1
Nathan Bubna <nathan@esha.com>
2016-05-24 15:23:24 -0700

    allow api change via env var

--- heroku/server.js
+++ heroku/server.js
@@ -4,9 +4,7 @@ var express = require('express');
 var logfmt = require('logfmt');
 var request = require('request');
 var app = express();
-//var api = 'http://api.esha.com';
-var api = 'http://api-staging.esha.com';
-//var api = 'http://localhost:8080';
+var api = process.env.API || 'http://api.esha.com';
 var key = process.env.APIKEY;
 
 function toApi(url) {
